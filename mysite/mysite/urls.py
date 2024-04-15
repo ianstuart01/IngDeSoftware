2925db62f86cbd88233b607ddde6d1f2ecceee4c
ianstuart01 <ianstuart2006@gmail.com>
2024-04-15 02:04:24 -0300

    [Parte 4.1] Escribo formulario

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,10 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+"""
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+]
+"""
+
+from django.contrib import admin
+from django.urls import include, path
+from polls import views  # Importa las vistas de la aplicación polls
+
+urlpatterns = [
+    path("", views.index, name="index"),  # Ruta raíz del sitio que apunta a la vista index de polls
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
